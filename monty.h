938a3941d03e4ade516218a917bfb831b49a17d2
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-15 11:27:12 +0000

    pop and header file update

--- monty.h
+++ monty.h
@@ -1,54 +1,36 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-
-/*LIBRARIES */
+/*LIBRARIES*/
 #define _POSIX_C_SOURCE 200809L
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <errno.h>
 
-
-/*STRUCT DEFINITIONS*/
-
+/*struct templates*/
 /**
- * struct stack_s -doubly linked list representation of a stack or queue
+ * struct stack_s - dll implementation of a stack 
  * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- * 
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
-
-
-/**
- * struct instructions_s opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
+ * @next: link to next element in stack
+ * @prev: link to prev element in stack
  *
- * Description: opcode and its function 
- * for stack, queues, LIFO, FIFO
+ * Description- dll node structure for monty project
  */
-typedef struct instruction_s{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+typedef struct stack_s{
+	int n;
+	struct stack_s *next;
+	struct stack_s *prev;
+} stack_t;
 
 /**
  * global_vars - holds global variables
  * @mfile: file pointer for monty file
- * @optoken: user inputed instruction
- * @arg: value for push in string form
+ * @inst: user inputed instruction
+ * @arg: value for push et al in string form
  * @value: value for push et al in integer form
  * @buffer: holds each line per time
- * @status: exit flag for program
+ * @status: exit status for program
  */
 typedef struct global_vars{
 	FILE *mfile;
@@ -60,18 +42,39 @@ typedef struct global_vars{
 } g_struct;
 
 
-/*GLOBAL VARIABLES*/
+/**
+ * struct instructions_s - opcode  and  its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function for monty project
+ */
+typedef  struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/*GLOBAL VARIABLE*/
 extern g_struct holder;
 
 /*FUNCTION PROTOTYPES*/
 void check_and_open(int arg_no, char *filepath);
-void(*o_s(unsigned int line_number))(stack_t **stack, unsigned int line_number);
+void (*o_s(unsigned int line_number))(stack_t **stack, unsigned int line_number);
 void stack_setup(stack_t **stack);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void my_div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 void cleanup(stack_t *stack);
 
--- pop.c
+++ pop.c
@@ -1,30 +1,20 @@
-#include "monty.h"
+#include "monty2.h"
 /**
- * pop - removes the top element in a stack
- * @stack: pointer to top of stack
- * @line_number: line of opcode
- */
+* pop - removes the top element from the stack
+* @stack: pointer to the top of the stack
+* @line_number: current line number of monty file
+*/
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+    stack_t *temp = *stack;
 
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		holder.status = 0;
-	}
-
-	if ((*stack)->next != NULL)
-	{
-		temp = (*stack)->next;
-		temp->prev = NULL;
-		free(*stack);
-		*stack = temp;
-	}
-	else
-	{
-		free(*stack);
-		*stack = NULL;
-	}
-
+    if ((*stack) == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n",line_number);
+        free(holder.buffer);
+        exit(EXIT_FAILURE);
+    }
+    (temp->next)->prev = NULL;
+    *stack = temp->next;
+    free(temp);
 }
