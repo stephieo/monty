e800b9abae92a274623bf24b9bce5d96f5bc5cfe
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-15 11:20:17 +0000

    stack tear down

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - Does nothing
+ * @stack: pointer to top of stack
+ * @line_number: line of opcode
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	(void) stack;
+}
