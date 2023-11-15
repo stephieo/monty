e437d2c1ba38ba19022884edd4217315a80864ef
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-15 11:16:06 +0000

    set up and teardown code

---
+++ check_and_open.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * check_and_open - checks for correct args and opens montyfile
+ * @arg_no: argument count
+ * @filepath: filepath of monty file
+ *
+ */
+void check_and_open(int arg_no, char *filepath)
+{
+	if (arg_no != 2)
+	{	fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	holder.mfile = fopen(filepath, "r");
+	if (holder.mfile == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filepath);
+		exit(EXIT_FAILURE);
+	}
+
+}
---
+++ cleanup.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * cleanup - collection of cleanup function calls
+ * @stack: head of stack
+ */
+void cleanup(stack_t *stack)
+{
+	free(holder.buffer);
+	fclose(holder.mfile);
+	free_stack(stack);
+}
