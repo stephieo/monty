271534dce1064344f73e725b109d1f935e0465da
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-15 12:16:16 +0000

    betty fix: pall

--- pall.c
+++ pall.c
@@ -6,19 +6,20 @@
 */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = *stack;
-    
-    (void)line_number;
+	stack_t *temp = *stack;
 
-    if (*stack == NULL)
-        return;
+	(void)line_number;
 
-    while (temp->next != NULL)
-    {
-        printf("%d\n", temp->n);
-        temp = temp->next;
-    }
-    printf("%d\n", temp->n);
-    printf("0\n");
+	if (*stack == NULL)
+		return;
+
+	while (temp->next != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+
+	printf("%d\n", temp->n);
+	printf("0\n");
 
 }
