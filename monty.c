1d35f242916e6547d0b6d780f7203cf28ab0c277
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-11-19 00:15:06 +0000

    modified: monty.c output; nop.c  betty error

--- monty.c
+++ monty.c
@@ -15,12 +15,12 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	if ((holder.mfile = fopen(argv[1], "r")) == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (holder.status)
--- nop.c
+++ nop.c
@@ -1,6 +1,8 @@
 #include "monty.h"
 /**
-* nop -
+* nop - does nothing
+* @stack: pointer to head of stack
+* @line_number: line number of monty file
 */
 void nop(stack_t **stack, unsigned int line_number)
 {
